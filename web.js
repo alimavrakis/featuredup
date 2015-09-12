19d6652dbbee8e905d54ea4898018c7fb8f27861
Ali Mavrakis <ali.mavrakis@gmail.com>
2015-09-12 16:10:40 -0500

    updated web.js

--- web.js
+++ web.js
@@ -4,5 +4,5 @@ var morgan = require('morgan');
 var app = express();
 
 app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + "/home/ali/docs/featuredup" + "/dist"));
+app.use(gzippo.staticGzip("" + "featuredup" + "/dist"));
 app.listen(process.env.PORT || 5000);
